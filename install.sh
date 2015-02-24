2d0b4d7bf94ccb9811a7ca3d53ef770081772979
Adam Tolley <refactorized@gmail.com>
2015-02-23 23:21:04 -0500

    dot files ftw

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
-ln bash_profile /Users/adam/.bash_profile
+if [ -k ~/.bash_profile ] ; then
+  echo 'breaking old bash profile symlink'
+elif [ -f ~/.bash_profile ] ; then
+  echo 'moving old bash to ._bash_profile.old'
+  cp -f ~/.bash_profile ~/._bash_profile.old
+  rm -f ~/.bash_profile
+fi
+ln -Ffhs `dirname $0`/bash_profile ~/.bash_profile
+echo 'symlinked new ~/.bash_profile, sourcing it now.'
+source ~/.bash_profile
