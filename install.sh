89911f4f95d23dbfc5a151752a58a8fe1e37cc3f
Adam Tolley <refactorized@gmail.com>
2016-07-23 15:22:28 -0400

    actually working install and no more summon-octocat

--- install.sh
+++ install.sh
@@ -1,10 +1,27 @@
-if [ -k ~/.bash_profile ] ; then
-  echo 'breaking old bash profile symlink'
-elif [ -f ~/.bash_profile ] ; then
-  echo 'moving old bash to ._bash_profile.old'
-  cp -f ~/.bash_profile ~/._bash_profile.old
-  rm -f ~/.bash_profile
+if [ -L ~/.sh ] ; then
+  rm -f ~/.sh
 fi
-ln -Ffhs `dirname $0`/bash_profile ~/.bash_profile
-echo 'symlinked new ~/.bash_profile, sourcing it now.'
-source ~/.bash_profile
+if [ ! -e ~/.sh ] ; then
+  ln -Ffhs `pwd` ~/.sh
+fi
+if [ ! -e ~/.local/bin/bashmarks.sh ] ; then
+  git clone git://github.com/huyng/bashmarks.git
+  cd bashmarks
+  make install
+  cd ..
+  rm -rf bashmarks
+fi
+if [ -e ~/.bash_profile ] ; then
+  if [ -L ~/.bash_profile ] ; then
+    echo 'breaking old bash profile symlink'
+    rm -f ~/.bash_profile
+  else [ -f ~/.bash_profile ]
+    echo 'moving old bash to ._bash_profile.old'
+    cp -f ~/.bash_profile ~/._bash_profile.old
+    rm -f ~/.bash_profile
+  fi
+fi
+  ln -Ffhs `pwd`/bash_profile ~/.bash_profile
+  echo `pwd`/bash_profile ~/.bash_profile
+  echo 'symlinked new ~/.bash_profile, sourcing it now.'
+  source ~/.bash_profile
